4446944375d4900fd3908d55859cffea1ec3e271
Daniil Fedotov <fedot13887@gmail.com>
2022-12-24 18:24:55 +0300

    add dz

---
+++ tower/program.java
@@ -0,0 +1,7 @@
+public class program {
+    public static void main(String[] args) {
+        int number = methods.inputNum("Введите количество дисков: ");
+        methods.tower(number, "источник", "приемник", "вспомогательный");
+    }
+
+}
